9794fb5503990bd5ec59f1a8bd565f3b17ca0abb
Patrolin <patrik@balas.cz>
2021-03-03 21:45:24 +0100

    ew

--- vscode/SAVE.sh
+++ vscode/SAVE.sh
@@ -1,2 +1,5 @@
+#code_path = "~/.config/Code\ -\ OSS"
+code_path="/mnt/c/Users/patri/AppData/Roaming/Code"
 code --list-extensions | sed 's/\(.*\)/\&\& code --install-extension \1/' | sed '1 s/\&\& //' > extensions.sh
-cat ~/.config/Code\ -\ OSS/User/settings.json > settings.json
+cat $code_path/User/settings.json > settings.json
+cat $code_path/User/keybindings.json > keybindings.json
